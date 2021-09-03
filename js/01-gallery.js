eb9361eb94951828468f1ad5195645363c67700d
Karolina Ścieszko <karolinascieszko@gmail.com>
2021-09-03 10:47:29 +0200

    update

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,53 +2,45 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 console.log(galleryItems);
 
-const qs = (selector) => document.querySelector(selector);
-const qsa = (selector) => document.querySelectorAll(selector);
+const qs = selector => document.querySelector(selector);
+const qsa = selector => document.querySelectorAll(selector);
 
-
-const galleryBox = qs(".gallery");
+const galleryBox = qs('.gallery');
 console.log(galleryBox);
 
 //ADDING GALLERY
 
 const images = galleryItems
-    .map(
-        item =>
-            `<div class = "gallery__item">
+  .map(
+    item =>
+      `<div class = "gallery__item">
             <a class = "gallery__link" href="${item.original}">
             <img class="gallery__image" src="${item.preview} "data-source="${item.original}" alt="${item.description}"/> 
-            </a></div>`
-    )
-    .join('');
+            </a></div>`,
+  )
+  .join('');
 
-galleryBox.insertAdjacentHTML("afterbegin", images);
+galleryBox.insertAdjacentHTML('afterbegin', images);
 console.log(images);
 
-
-const galleryImages = qsa(".gallery__image");
-console.log("galleryImages");
-
+const galleryImages = qsa('.gallery__image');
+console.log('galleryImages');
 //ADD GALLERY
-
-
 galleryImages.forEach(item => {
-    item.addEventListener("click", event => {
-        event.preventDefault();
-
-// CREATE MODAL WINDOW
-        
-        let modalWindow = basicLightbox.create(`<img src="${item.dataset.source}" alt="${item.alt}">`)
-        modalWindow.show();
-
-//ESC CLOSE
-
-        if (modalWindow.visible() === true) {
-            document.addEventListener("keydown", event => {
-                if (event.key === "Escape") {
-                    modalWindow.close();
-                }
-            })
+  item.addEventListener('click', event => {
+    event.preventDefault();
+    // CREATE MODAL WINDOW
+    let modalWindow = basicLightbox.create(
+      `<img src="${item.dataset.source}" alt="${item.alt}">`,
+    );
+    modalWindow.show();
+    //ESC CLOSE
+    if (modalWindow.visible() === true) {
+      document.addEventListener('keydown', event => {
+        if (event.key === 'Escape') {
+          modalWindow.close();
         }
-    })
-})
-
+      });
+    }
+  });
+});
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,34 +1,25 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 console.log(galleryItems);
-const qs = (selector) => document.querySelector(selector);
-const qsa = (selector) => document.querySelectorAll(selector);
-
-//ADDING GALLERY ITEMS
-
-const galleryBox = qs(".gallery");
+const qs = selector => document.querySelector(selector);
+const qsa = selector => document.querySelectorAll(selector);
+//ADD GALLERY ITEMS
+const galleryBox = qs('.gallery');
 console.log(galleryBox);
-
-
 const images = galleryItems
-    
-    .map(
-        item =>
-            `<a class = "gallery__item" href="${item.original}">
+  .map(
+    item =>
+      `<a class = "gallery__item" href="${item.original}">
             <img class="gallery__image" src="${item.preview}"  alt="${item.description}" title="${item.description}"/> 
-            </a>`
-    )
-    .join('');
+            </a>`,
+  )
+  .join('');
 
-galleryBox.insertAdjacentHTML("afterbegin", images);
+galleryBox.insertAdjacentHTML('afterbegin', images);
 console.log(images);
-
 //ADDITIONAL OPTIONS
-
 let lightbox = new SimpleLightbox('.gallery a', {
-    captionsData: "alt",
-    captionDelay: 250,
+  captionsData: 'alt',
+  captionDelay: 250,
 });
 console.log(lightbox);
-
-
